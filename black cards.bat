b66935c1bc7dc22ddb14c90520edc1a81e1ebd46
Arion Roberto Krause <arionkrause@gmail.com>
2016-01-08 19:10:55 -0200

    Initialized repository.

---
+++ black cards.bat
@@ -0,0 +1,1 @@
+ruby\bin\ruby.exe "cards agains humanity.rb" --black "black cards.txt" --output "black cards.pdf"
---
+++ white cards.bat
@@ -0,0 +1,1 @@
+ruby\bin\ruby.exe "cards agains humanity.rb" --white "white cards.txt" --output "white cards.pdf"
